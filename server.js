b83c37d17771361c4e9c18618036c5a80f66060f
weicewang <weicewang0712@gmail.com>
2025-12-09 12:09:05 +1100

    Add the Create, Update and Delete page

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 const Fruit = require("./models/fruit.js");
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan"); //new
 
 // GLOBAL VARIABLE
 const app = express();
@@ -22,14 +24,47 @@ app.use(
   })
 );
 
-// INDEX ROUTES
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
+
+// HOME ROUTES
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("home.ejs");
 });
 
 // NEW ROUTE
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
+});
+
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find({});
+  console.log(allFruits); // log the list
+  res.render("fruits/index.ejs", {
+    // express identify the views, but cannot get deeeper folders
+    fruits: allFruits,
+  });
+});
+
+// DELETE ROUTE
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+// UPDATE ROUTE
+app.put("/fruits/:fruitId", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  res.redirect(`/fruits/${req.params.fruitId}`);
 });
 
 // CREATE ROUTE
@@ -41,7 +76,26 @@ app.post("/fruits", async (req, res) => {
   }
   console.log(req.body);
   await Fruit.create(req.body);
-  res.redirect("/fruits/new");
+  res.redirect("/fruits");
+});
+
+// server.js
+
+// EDIT ROUTE
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log(foundFruit);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+// SHOW ROUTE
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log(foundFruit);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
 // APP LISTENER
